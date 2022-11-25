90fa0b8df045af095cc9ccd8826e2685d52c7b3f
JeanBaptiste Allemandou <jeanbaptiste.allemandou@gmail.com>
2022-11-25 16:27:41 +0100

    push on heroku photo loader added

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -30,6 +30,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.fetch(:list, {}).permit(:name)
+    params.fetch(:list, {}).permit(:name, :photo)
   end
 end
