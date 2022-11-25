8ec69450f38267ca2ff2e8c19c84f256a59daf84
JeanBaptiste Allemandou <jeanbaptiste.allemandou@gmail.com>
2022-11-25 12:13:35 +0100

    update with validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ApplicationRecord
-  # validates :title, field: true
-  # validates :title, uniqueness: true
   has_many :bookmarks, dependent: :destroy
   has_many :lists, through: :bookmarks
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+
 end
